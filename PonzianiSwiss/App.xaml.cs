35ddc0e847fa0b2ca75584567f726667c6d94c98
guentherc <chr.guenther@web.de>
2022-08-04 17:15:49 +0200

    Version 0.5.0

--- PonzianiSwiss/App.xaml.cs
+++ PonzianiSwiss/App.xaml.cs
@@ -18,7 +18,7 @@ namespace PonzianiSwiss
     /// </summary>
     public partial class App : Application
     {
-        public const string VERSION = "0.4.1";
+        public const string VERSION = "0.5.0";
 
         /// <summary>
         /// Gets the current <see cref="App"/> instance in use
